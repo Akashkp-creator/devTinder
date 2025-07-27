180d7494727971e7c667e7ae9be3f45197ed5ae7
Akashkp-creator <akp979760@gmail.com>
2025-07-27 07:10:57 +0530

    exploring get,post,put and delete

--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {
-  console.log("Namaste from Akash");
-  console.log(`${req.url}`);
-  res.send("namaste from Akash");
+// app.use("/hello2", (req, res) => {
+//   res.send("Hyyy this is hello2 testing");
+// });
+
+// app.use("/", (req, res) => {
+//   console.log("Namaste from Akash");
+//   console.log(`${req.url}`);
+//   res.send("namaste from Akash");
+// });
+app.post("/test1", (req, res) => {
+  res.send("Hi this is test1... for post.");
 });
-app.use("/test1", (req, res) => {
-  console.log("Hi this is testing....");
-  res.send("nameste this is testing....");
+app.get("/test1", (req, res) => {
+  res.send("hyyy this is get test1");
 });
 
+app.put("/test1", (req, res) => {
+  res.send("hyyy this is put test1");
+});
+
+app.delete("/test1", (req, res) => {
+  res.send("Hyyy this test1 from delete");
+});
 app.listen(3000, () => {
   console.log("Server is listening to the port 3000...");
 });
