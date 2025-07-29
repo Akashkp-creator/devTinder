de643a1460c41c9cb29c45739aa3be3d9f7a5068
Akashkp-creator <akp979760@gmail.com>
2025-07-29 14:53:52 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const app = express();
+const { connectDB } = require("./config/database");
+const { User } = require("./models/user");
 
-app.get("/admin/getAllData", (req, res) => {
-  console.log("Admin Data ");
-  throw new Error("i am an error");
-  // res.send("This is admin Data");
-});
-// here the order is very important and always write the below wild card at the end...so that it will handle the error
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    res.status(500).send("Something went wrong");
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Janvi",
+    lastName: "K M",
+    emailId: "janvi@janvi.com",
+    password: "123@123",
+    gender: "female",
+  });
+  try {
+    await user.save();
+    res.send("User Added Successfully ");
+  } catch (error) {
+    res.status(400).send("Error in saving the user:" + error.message);
   }
 });
-app.listen(3000, () => {
-  console.log("Server is listening to the port 3000...");
-});
+
+connectDB()
+  .then(() => {
+    console.log("Established connection successfully with DATABASE.....");
+    app.listen(3000, () => {
+      console.log("Server is listening to the port 3000...");
+    });
+  })
+  .catch((err) => {
+    console.log("Not connected with the DATABASE");
+  });
