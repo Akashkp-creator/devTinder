a446681bb53418ecf9382768aa7cdaa4b8908ec5
Akashkp-creator <akp979760@gmail.com>
2025-07-30 12:44:08 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -16,6 +16,55 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// API to GET user by email
+app.get("/user", async (req, res) => {
+  console.log(req.body);
+  try {
+    const user = await User.findOne({ firstName: req.body.firstName });
+    // const user = await User.findOne({ emailId: req.body.emailId });
+
+    if (!user) {
+      return res.status(404).send("NO user found");
+    }
+    res.status(200).send(user);
+  } catch (error) {
+    console.log("something went wrong in finding the user");
+    res
+      .status(500)
+      .send("something went wrong in finding the user " + error.message);
+  }
+});
+// API to Feed API -GET   /feed -get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      return res.status(404).send("no users found");
+    }
+    res.status(200).send(users);
+  } catch (error) {
+    res
+      .status(500)
+      .send("something went wrong in finding the users " + error.message);
+  }
+});
+
+// API - GET to findById
+app.get("/getByUserId", async (req, res) => {
+  console.log(req.body);
+  try {
+    const dataById = await User.findById({ _id: req.body._id });
+    if (!dataById) {
+      return res.status(404).send("invalid User Id");
+    }
+    res.status(200).send(dataById);
+  } catch (error) {
+    res
+      .status(500)
+      .send("something went wrong in finding the user " + error.message);
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Established connection successfully with DATABASE.....");
