a56c6542fac96d2dea95158726a6a843e2b69602
Akashkp-creator <akp979760@gmail.com>
2025-08-01 06:36:42 +0530

    schema validations

--- src/app.js
+++ src/app.js
@@ -64,6 +64,66 @@ app.get("/getByUserId", async (req, res) => {
       .send("something went wrong in finding the user " + error.message);
   }
 });
+// API to delete an user
+app.delete("/user", async (req, res) => {
+  // const userId = req.body.userId;
+  try {
+    // can also use Model.findByIdAndDelete() only for id
+    // const deletedUser = await User.findOneAndDelete({ _id: userId });
+    // short hand for { _id: userId } can also be used as userId
+    // const deletedUser = await User.findOneAndDelete(userId);
+    // const deletedUser = await User.findOneAndDelete(req.body.lastName);
+    // the short hand is in above line
+    const deletedUser = await User.findOneAndDelete({
+      lastName: req.body.lastName,
+    });
+    res.send(`${deletedUser} is deleted`);
+  } catch (error) {
+    res
+      .status(500)
+      .send("something went wrong in deleting the user " + error.message);
+  }
+});
+
+// API - to Update a user by id
+app.patch("/user", async (req, res) => {
+  const newValue = req.body;
+  console.log(newValue);
+  try {
+    const updatedValue = await User.findByIdAndUpdate(
+      { _id: req.body.userId },
+      newValue,
+      { returnDocument: "after", runValidators: true }
+    );
+    console.log(updatedValue);
+    res.send("user updated successfully");
+  } catch (error) {
+    res
+      .status(500)
+      .send("something went wrong in updating the user " + error.message);
+  }
+});
+
+// api- update user by emailId
+app.patch("/userUpdateByEmail", async (req, res) => {
+  const newValue = req.body;
+  console.log(newValue);
+  try {
+    const updatedValue = await User.findOneAndUpdate(
+      {
+        emailId: req.body.emailId,
+      },
+      newValue,
+      { returnDocument: "after" }
+    );
+    console.log(updatedValue);
+    res.send("user updated successfully using emailId");
+  } catch (error) {
+    res
+      .status(500)
+      .send("something went wrong in updating the user " + error.message);
+  }
+});
 
 connectDB()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,57 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minlength: [4, "First Name must be at least 4 characters long"],
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: [8, "Password must be at least 8 characters long"],
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: [18, "Must be at least 18"],
+      max: [60, "Max age is 80"],
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    skills: {
+      type: [String],
+      default: ["React", "Node.js", "javascript"],
+    },
+    about: {
+      type: String,
+      default: `Hi there I'm the Happiest in the world`,
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
